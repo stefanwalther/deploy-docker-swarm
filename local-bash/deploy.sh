1ced4323502389d50a74c9ed2548cd1dda8918fd
Stefan Walther <Stefan.Walther@qlik.com>
2017-03-07 09:36:04 -0500

    feat(local-bash): Deploy script

---
+++ local-bash/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Load the required scripts
+SCRIPT_DIR=`dirname "$BASH_SOURCE"`
+source $SCRIPT_DIR/lib/error-trap.sh
+source $SCRIPT_DIR/lib/constants.sh
+source $SCRIPT_DIR/config.sh
+source $SCRIPT_DIR/lib/03-swarm-deploy.sh
+
+deploy
--- local-bash/lib/03-swarm-deploy.sh
+++ local-bash/lib/03-swarm-deploy.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+
+function deploy() {
+
+  docker-machine scp ./docker-stack.yml manager-1:/home/docker/docker-stack.yml
+  docker-machine ssh manager-1 cd /home/docker;
+  docker-machine ssh manager-1 docker stack deploy --compose-file docker-stack.yml vote
+
+}
+
