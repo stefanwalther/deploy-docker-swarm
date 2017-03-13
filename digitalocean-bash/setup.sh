daae15ff4d6aea0fc798863783de134a5aef9dea
Stefan Walther <Stefan.Walther@qlik.com>
2017-03-13 09:47:30 -0400

    refactor(do-bash): Initial commit

---
+++ digitalocean-bash/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Options here: https://docs.docker.com/machine/drivers/digital-ocean/
+docker-machine create --driver digitalocean \
+  --digitalocean-access-token $DO_TOKEN
+  --digitalocean-image docker-16-04
+  --digitalocean-region "fra1"
+  --digitalocean-size "1gb"
+  --digitalocean-ipv6 false
+  --digitalocean-private-networking true
+  docker-sandbox
--- do-bash/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker-machine create --driver digitalocean --digitalocean-access-token $DO_TEST_TOKEN docker-sandbox
