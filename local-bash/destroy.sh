0199ed55b8ebf095b998fa22f33dae26ce703a30
Stefan Walther <Stefan.Walther@qlik.com>
2017-03-15 16:17:03 -0400

    refactor(local-bash): remove unnecessary deps

--- local-bash/destroy.sh
+++ local-bash/destroy.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR=`dirname "$BASH_SOURCE"`
 source $SCRIPT_DIR/lib/error-trap.sh
 source $SCRIPT_DIR/lib/constants.sh
 source $SCRIPT_DIR/config.sh
-source $SCRIPT_DIR/lib/03-swarm-deploy.sh
 
 function destroy_managers() {
   for i in $(seq 1 $NUM_MANAGERS); do
