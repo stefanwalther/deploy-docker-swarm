dde19e55df27e20a852a52c3aa2c1487bfbec5f5
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-05-30 18:28:01 +0200

    fix(local-vagrant): add config-file option for destroy.sh, remove unnecessary destroy files

--- local-vagrant/destroy.prod.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-vagrant -- destroy -f
-
-rm -rf ./.vagrant/
--- local-vagrant/destroy.sh
+++ local-vagrant/destroy.sh
@@ -1,5 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-vagrant --config-file="./configs/config.yml" destroy -f
+# defaults
+CONFIG_FILE=./configs/config.yml
+
+# https://stackoverflow.com/a/28466267/120981
+while getopts c:-: arg; do
+  case $arg in
+    c )  CONFIG_FILE="$OPTARG" ;;
+    - )  LONG_OPTARG="${OPTARG#*=}"
+         case $OPTARG in
+           config-file=?* )   CONFIG_FILE="$LONG_OPTARG" ;;
+           config-file*   )   echo "No arg for --$OPTARG option" >&2; exit 2 ;;
+           '' )               break ;; # "--" terminates argument processing
+           * )                echo "Illegal option --$OPTARG" >&2; exit 2 ;;
+         esac ;;
+    \? ) exit 2 ;;            # getopts already reported the illegal option
+  esac
+done
+shift $((OPTIND-1))           # remove parsed options and args from $@ list
+
+
+# -- main processing
+vagrant --config-file=${CONFIG_FILE} destroy -f
 
 rm -rf ./.vagrant/
+
